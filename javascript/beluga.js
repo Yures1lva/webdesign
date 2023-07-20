8f122afd0567488f7de7cbfa86655b7f0934a399
Yures1lva <yure.siilva9@gmail.com>
2023-07-20 11:18:57 -0300

    novos exemplos

---
+++ javascript/beluga.js
@@ -0,0 +1,12 @@
+function calcularMedia() {
+  var qtd = arguments.length;
+  var total = 0;
+  var x = 0;
+
+  while (typeof arguments[x] === "number") {
+    total += arguments[x];
+    x++;
+  }
+  console.log("o total é " + total);
+  return total / qtd;
+}
