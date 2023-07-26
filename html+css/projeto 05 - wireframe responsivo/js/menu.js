f6796e2937314bf357dfb210bb858e15bb8a2853
Yures1lva <yure.siilva9@gmail.com>
2023-07-26 10:29:35 -0300

    retirando hamburguer por conta de bugs

---
+++ html+css/projeto 05 - wireframe responsivo/js/menu.js
@@ -0,0 +1,10 @@
+// (function () {
+//   "use strict";
+
+//   var $html = document.querySelector("html");
+//   var $btn = document.querySelector(".mobile-menu");
+
+//   $btn.addEventListener("click", function () {
+//     $html.classList.toggle("menu-opened");
+//   });
+// })();
