592e4dc112b42cbb6e70e79276b01001bffa2422
Yures1lva <yure.siilva9@gmail.com>
2023-08-03 14:12:59 -0300

    manuteção no menu hamburguer

--- javascript/projetoFinal/js/menu.js
+++ javascript/projetoFinal/js/menu.js
@@ -1,4 +1,4 @@
-function Menu(config) {
+function menu(config) {
   this.nav =
     typeof config.container === "string"
       ? document.querySelector(config.container)
@@ -12,6 +12,7 @@ function Menu(config) {
   this.maxWidth = config.widthEnable || false;
 
   this.btn.revomeAttrinbute("style");
+  closeMenu();
 
   var _opened = false;
   var _this = this;
@@ -19,7 +20,7 @@ function Menu(config) {
   this.btn.addEventListener("click", openClose);
 
   function openClose() {
-    if (!opened) {
+    if (!_opened) {
       openMenu();
     } else {
       closeMenu();
@@ -27,10 +28,29 @@ function Menu(config) {
   }
 
   function openMenu() {
+    var _top = this.nav.getBoundingClientReact().top + "px";
+    var _style = {
+      maxHeigth: "calc(100vh - " + top + ")",
+      overflow: "hidden",
+    };
+
+    applyStyleToNav(_style);
     _opened = true;
   }
 
+  function applyStyleToNav(_style) {
+    Object.keys(_style).forEach((stl) => {
+      _this.nav._style[stl] = _style[stl];
+    });
+  }
+
   function closeMenu() {
+    var _style = {
+      maxHeigth: "0px",
+      overflow: "hidden",
+    };
+
+    applyStyleToNav(_style);
     _opened = false;
   }
 }
--- javascript/projetoFinal/js/script.js
+++ javascript/projetoFinal/js/script.js
@@ -3,6 +3,9 @@
   $body.classList.remove("no-js");
   $body.classList.add("js");
 
+  var $btnMenu = document.querySelector(".header__btnMenu");
+  $btnMenu.removeAttribute("style");
+
   var $menu = new menubar({
     container: "header__nav",
     toggleBtbn: "header__btnMenu",
