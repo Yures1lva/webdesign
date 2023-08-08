f3e3616baa7a4e2a897b32aaf69b72e2cbc76553
Yures1lva <yure.siilva9@gmail.com>
2023-08-08 16:50:20 -0300

    só merdas

---
+++ javascript/projetoFinal/js/carousel.js
@@ -0,0 +1,30 @@
+// function Carousel(config) {
+//   console.log("container: ", this.cont);
+//   this.container =
+//     typeof config.container === "string"
+//       ? document.querySelector(config.container)
+//       : config.container;
+
+//   this.itens =
+//     typeof config.itens === "string"
+//       ? this.container.querySelectorAll(config.itens)
+//       : config.itens;
+
+//   this.btnPrev =
+//     typeof config.btnPrev === "string"
+//       ? this.container.querySelectorAll(config.btnPrev)
+//       : config.btnPrev;
+
+//   this.btnNext =
+//     typeof config.btnNext === "string"
+//       ? this.container.querySelectorAll(config.btnNext)
+//       : config.btnNext;
+
+//   var _this = this;
+
+//   init();
+
+//   function init() {
+//     var _show = _this.container.querySelectorAll(".show");
+//   }
+// }
--- javascript/projetoFinal/js/script.js
+++ javascript/projetoFinal/js/script.js
@@ -1,10 +1,46 @@
 (function () {
   var $btnMobile = document.getElementById("btn-mobile");
+  var $slide = document.querySelector(".slide .slideshow");
+  var $itens = document.querySelector("figure");
+  var $btnPrev = document.querySelector(".prev");
+  var $btnNext = document.querySelector(".next");
+  var _currentSlider = 0;
 
   $btnMobile.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     var $nav = document.getElementById("nav");
     $nav.classList.toggle("active");
+  }
+
+  init();
+
+  function init() {
+    var _show = $slide.querySelectorAll(".show");
+
+    Array.prototype.forEach.call(_show, function (sh) {
+      sh.classList.remove("show");
+    });
+    $itens.classList.add("show");
+    $btnNext.removeAttribute("style");
+    $btnPrev.removeAttribute("style");
+
+    addlistener();
+  }
+
+  function addlistener() {
+    $btnNext.addEventListener("click", function () {
+      _currentSlider++;
+      showSlide();
+    });
+    $btnPrev.addEventListener("click", function () {
+      _currentSlider--;
+      showSlide();
+    });
+  }
+
+  function showSlide() {
+    var qtd = $itens.lenght;
+    var slider = _currentSlider % qtd;
   }
 })();
