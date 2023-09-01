90365bc9d0c759932a4ac147eb8cb24a67bc810f
Yures1lva <yure.siilva9@gmail.com>
2023-09-01 15:21:09 -0300

    estudando animação ao scrolar

---
+++ avançado/scrollReveal/scrollReveal.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal(".anime-down ", {
+  origin: "top",
+  delay: 300,
+  distance: "-100px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".anime-left ", {
+  origin: "left",
+  delay: 500,
+  distance: "100px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".anime-right ", {
+  origin: "right",
+  delay: 500,
+  distance: "100px",
+  reset: true,
+});
